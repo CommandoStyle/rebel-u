7671e8185c285e80b8f15e0062d942364087ecc5
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-04-25 21:06:25 +0700

    Update mrebelImages.js

--- js/mr-ebel/mrebelImages.js
+++ js/mr-ebel/mrebelImages.js
@@ -1,6 +1,4 @@
-import gsap from 'gsap';
-
-const mrebelImages = () => {
+export const mrebelImages = () => {
       
 // Menu items image reveal hover
 
@@ -210,5 +208,3 @@ window.addEventListener('mousemove', ev => mousepos = getMousePos(ev));
 new subsLink(subsEl);
 
     }
-
-  export default mrebelImages;
