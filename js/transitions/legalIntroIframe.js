d44fd1ee1dceaded4ce88d8f8cdcb6ad6e8dd85c
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-04-25 21:24:20 +0700

    Update legalIntroIframe.js

--- js/transitions/legalIntroIframe.js
+++ js/transitions/legalIntroIframe.js
@@ -1,6 +1,4 @@
-import gsap from 'gsap';
-
-const legalIntroIframe = (container) => {
+export const legalIntroIframe = (container) => {
 
     gsap.set(".legal-reveal", { x: -30, opacity: 0 });
     gsap.set(['.home-logo', ".menu-txt", '.beta__txt', '.hero-info__marquee-wrap', '.legal-lottie__contain'], {autoAlpha: 1});
@@ -20,5 +18,3 @@ legalIntroIframe
 
 
 }
-    
-export default legalIntroIframe;
