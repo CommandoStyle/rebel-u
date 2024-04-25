dc31fad44918c9f4774f5f8672926fa5b30c1ea3
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-04-25 20:52:09 +0700

    Update initWebflow.js

--- js/global/initWebflow.js
+++ js/global/initWebflow.js
@@ -1,7 +1,4 @@
-
-// init WF interactions after barba transition - on Proposition 
-
-const initWebflow = () => {
+export const initWebflow = () => {
 
   const pageId = '64217a46f171f96acc4bd03b'; // may need to query data.next.html for data-wf-page attribute
   document.documentElement.setAttribute('data-wf-page', pageId);
@@ -10,6 +7,6 @@ const initWebflow = () => {
   window.Webflow && window.Webflow.ready();
   window.Webflow && window.Webflow.require( 'ix2' ).init();
   document.dispatchEvent( new Event( 'readystatechange' ) );
-  }
+ 
+}
 
-export default initWebflow;  
