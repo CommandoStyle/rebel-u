7b9f1d23f0ff5c2d320055bec899fd21858c506f
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-04-30 11:40:27 +0700

    Update smlArrowCta.js

--- js/global/smlArrowCta.js
+++ js/global/smlArrowCta.js
@@ -2,9 +2,13 @@ export const smlArrowCta = () => {
   
 gsap.set(".topic-nav__track-right", { xPercent: 0 });
 gsap.set(".topic-nav__track-left", { xPercent: 0 });
+gsap.set(".skip-btn-arrow__wrap", { yPercent: 0 });
+gsap.set(".skip-btn-arrow-up__wrap", { yPercent: 0 });
   
 let rArrows = gsap.utils.toArray('.topic-nav__next'); 
 let lArrows = gsap.utils.toArray('.topic-nav__prev'); 
+let dArrows = gsap.utils.toArray('.reb__skip-btn'); 
+let uArrows = gsap.utils.toArray('.reb__skip-up-btn'); 
   
 rArrows.forEach(function(el) { 
     
@@ -105,5 +109,108 @@ rArrows.forEach(function(el) {
       }    
     }  
   });
+
+  dArrows.forEach(function(el) { 
+    
+    let aTrackDown = el.querySelector(".skip-btn-arrow__wrap"); 
+    let aAnimationDown = null;
+    let isAhoveringDown = false;
+    
+    el.addEventListener("mouseenter", ArrowOnEnterD);  
+    el.addEventListener("mouseleave", ArrowOnLeaveD);
+    
+    function ArrowOnEnterD() {
+
+      let smlArrowD = gsap.timeline();
+      
+      isAhoveringDown = true;
+      
+      if (!aAnimationDown) {      
+        smlArrowD.fromTo(aTrackDown, { yPercent: 0}, { yPercent: 0, duration: 0.2})
+        .fromTo(aTrackDown, { yPercent: 100 }, {
+        yPercent: 0,
+        ease: "power1.easeOut",
+        duration: 0.4,
+          onComplete: () => {
+            aAnimationDown = null;
+            if (!isAhoveringDown) {            
+              ArrowOnLeaveD();
+            }
+          }
+        });
+      }    
+    }
+    
+    function ArrowOnLeaveD() {
+      
+      isAhoveringDown = false;
+      
+      if (!aAnimationDown) {            
+        aAnimationDown = gsap.to(aTrackDown, {
+          yPercent: 0,
+          duration: 0.01,
+           onComplete: () => {
+             aAnimationDown = null;
+             if (isAhoveringDown) {
+               ArrowOnEnterD();
+             }
+           }
+        });
+      }    
+    }  
+  });
+
+    uArrows.forEach(function(el) { 
+    
+    let aTrackUp = el.querySelector(".skip-btn-arrow-up__wrap"); 
+    let aAnimationUp = null;
+    let isAhoveringUp = false;
+    
+    el.addEventListener("mouseenter", ArrowOnEnterU);  
+    el.addEventListener("mouseleave", ArrowOnLeaveU);
+    
+    function ArrowOnEnterU() {
+
+      let smlArrowU = gsap.timeline();
+      
+      isAhoveringUp = true;
+      
+      if (!aAnimationUp) {      
+        smlArrowU.fromTo(aTrackUp, { yPercent: 0}, { yPercent: 0, duration: 0.2})
+        .fromTo(aTrackUp, { yPercent: 100 }, {
+        yPercent: 0,
+        ease: "power1.easeOut",
+        duration: 0.4,
+          onComplete: () => {
+            aAnimationUp = null;
+            if (!isAhoveringUp) {            
+              ArrowOnLeaveU();
+            }
+          }
+        });
+      }    
+    }
+    
+    function ArrowOnLeaveU() {
+      
+      isAhoveringUp = false;
+      
+      if (!aAnimationUp) {            
+        aAnimationUp = gsap.to(aTrackUp, {
+          yPercent: 0,
+          duration: 0.01,
+           onComplete: () => {
+             aAnimationUp = null;
+             if (isAhoveringUp) {
+               ArrowOnEnterU();
+             }
+           }
+        });
+      }    
+    }  
+  });
+
+
+
 
 }
