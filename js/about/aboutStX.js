d6ecb4169171261c8497f236db3abc210aff5578
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-05-01 16:20:00 +0700

    Update aboutStX.js

--- js/about/aboutStX.js
+++ js/about/aboutStX.js
@@ -2,6 +2,19 @@ export const aboutStX = () => {
 
 gsap.registerPlugin(ScrollTrigger);
 
+  const underlineLG = document.getElementById("reb-escape__underline");
+  const underlineLGdata = underlineLG.getAttribute("lottie-data");
+  underlineLG.load(underlineLGdata);
+  
+  ScrollTrigger.create({
+      trigger: ".escape-underline__wrap",
+      start: "top 80%",
+      onEnter: () => underlineLG.play(),
+      //onLeave: () => underlineLG.seek('0%'),
+      onEnterBack: () => underlineLG.play(),
+      onLeaveBack: () => underlineLG.seek('0%')
+  });
+
 var rotate = gsap.timeline({
     scrollTrigger:{
       trigger: ".about-x__track",
