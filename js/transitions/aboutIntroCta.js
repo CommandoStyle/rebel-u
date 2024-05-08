b7a7f9aa9a9b5c6cac458c753a3187aa3771ffb2
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-05-08 16:18:13 +0700

    Update aboutIntroCta.js

--- js/transitions/aboutIntroCta.js
+++ js/transitions/aboutIntroCta.js
@@ -3,9 +3,9 @@ export const aboutIntroCta = (container) => {
   gsap.registerPlugin(MorphSVGPlugin);
 
   gsap.set(".about-reveal", { x: -30, opacity: 0 });
-  gsap.set(['.home-logo', ".menu-txt", '.beta__txt', ".sml__square", '.hero-info__marquee-wrap', '.reb-globe-html'], {autoAlpha: 1});
+  gsap.set(['.home-logo', ".menu-txt", '.beta__txt', ".sml__square", '.hero-info__marquee-wrap', '.reb-globe-html', ".rblu-exit__img"], {autoAlpha: 1});
   gsap.set([".redact-cover-logo", ".redact-cover-beta", ".redact-cover-info", ".redact-cover-nav"], {scaleX: 0 });
-  gsap.set([".redact-cover-globe", ".redact-cover-your"], {scaleY: 0 });
+  gsap.set([".redact-cover-globe", ".redact-cover-your", ".rblu-exit__cover"], {scaleX: 0 });
   gsap.set('.about-hero__cm-bg', { yPercent: 0 });
   gsap.set('.u-txt', { autoAlpha: 1 });
   gsap.set(['.about-hero__cm-wrap', ".about-hero-logo__wrap"], { rotationY: -180 });
