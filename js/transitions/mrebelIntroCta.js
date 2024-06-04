23227a707217b7c956947eae886509db6839535f
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-06-04 16:28:23 +0700

    Update mrebelIntroCta.js

--- js/transitions/mrebelIntroCta.js
+++ js/transitions/mrebelIntroCta.js
@@ -5,7 +5,7 @@ gsap.set(['.home-logo', ".menu-txt", '.beta__txt', ".query-txt", ".query-away__t
 gsap.set([".redact-cover-logo", ".redact-cover-beta", ".redact-cover-query", ".redact-cover-nav", ".redact-cover-qa"], {scaleX: 0 });
 gsap.set('.ebel-42__wrap', { autoAlpha: 0 });
 gsap.set('.indie-advisory__img', { autoAlpha: 1 });
-gsap.set(['.redact-cover-advisory', '.redact-cover-42', ".rblu-exit__cover"], { scaleX: 0 });
+gsap.set(['.redact-cover-advisory', '.redact-cover-42'], { scaleX: 0 });
 gsap.set(".hero-scroll", { display: "none" });
 
 const circleXL = document.getElementById("hero__lottie-answer");
@@ -23,9 +23,6 @@ fortyTwo.load(fortyTwodata);
       .to(".mrebel-reveal", { delay: 1.1, duration: 1, x: 0, stagger: 0.03, ease: "expo.out" })
       .to(".mrebel-reveal", { duration: 0.4, opacity: 1, stagger: 0.03, ease: "linear" }, "<")
       .set(".menu-txt", { color: '#000000'}, "-=0.85")
-      .to(".rblu-exit__cover", { scaleX: 1, transformOrigin: "0% 100%", duration: 0.35, ease: "linear"}, "+=0.15")
-      .from(".rblu-exit__img", { autoAlpha: 0, duration: 0.001})
-      .to(".rblu-exit__cover", { scaleX: 0, transformOrigin: "100% 0%", duration: 0.35, ease: "linear"}, "+=0.01")
       .to(".redact-cover-42", { scaleX: 1, transformOrigin: "0% 100%", duration: 0.125, ease: "linear"}, "+=0.1")
       .to('.ebel-42__wrap', { autoAlpha: 1, duration: 0.001, onComplete: () => { circleXL.play(); }})
       .to(".redact-cover-42", { scaleX: 0, transformOrigin: "100% 0%", duration: 0.125, ease: "linear"})
