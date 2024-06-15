905dad0e69b5c31416a8d0ff639eaf20ad4ebc87
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-06-15 15:45:30 +0700

    Create responsiveText.js

---
+++ js/global/responsiveText.js
@@ -0,0 +1,42 @@
+export const responsiveText = () => {
+
+let mm = gsap.matchMedia();
+
+//iPad and Mobile
+mm.add("(max-width: 640px)", () => {
+  
+function clampBuilder(minWidthPx, maxWidthPx, minFontSize, maxFontSize) {
+  const root = document.querySelector("html");
+  const pixelsPerRem = Number(getComputedStyle(root).fontSize.slice(0, -2));
+
+  const minWidth = minWidthPx / pixelsPerRem;
+  const maxWidth = maxWidthPx / pixelsPerRem;
+
+  const slope = (maxFontSize - minFontSize) / (maxWidth - minWidth);
+  const yAxisIntersection = -minWidth * slope + minFontSize;
+
+  return `clamp(${minFontSize}rem, ${yAxisIntersection}rem + ${
+    slope * 100
+  }vw, ${maxFontSize}rem)`;
+}
+
+function calculateCh(element, fontSize) {
+  const zero = document.createElement("span");
+  zero.innerText = "0";
+  zero.style.position = "absolute";
+  zero.style.fontSize = fontSize;
+
+  element.appendChild(zero);
+  const chPixels = zero.getBoundingClientRect().width;
+  element.removeChild(zero);
+
+  return chPixels;
+}
+
+document.querySelectorAll(".is--bread").forEach((bread) => {
+  bread.style.fontSize = clampBuilder(320, 640, 0.5, 1);
+  bread.style.width = `${(320 / calculateCh(bread, "1rem")) * 1}ch`;
+ });
+});
+
+}
