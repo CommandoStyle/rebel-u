3e649cafa1f89d3f446ed050c1861898876c1bce
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-06-15 17:31:47 +0700

    Update responsiveText.js

--- js/global/responsiveText.js
+++ js/global/responsiveText.js
@@ -34,7 +34,7 @@ function calculateCh(element, fontSize) {
 }
 
 document.querySelectorAll(".p-full").forEach((pfull) => {
-  pfull.style.fontSize = clampBuilder(320, 480, 0.75, 1.125);
+  pfull.style.fontSize = clampBuilder(320, 480, 0.8, 1.2);
   pfull.style.width = `${(320 / calculateCh(pfull, "1rem")) * 0.9}ch`;
   });
   
