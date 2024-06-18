cf13221833f5fde43b9fcf2ae88e66640b96aba7
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-06-18 10:41:53 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -234,8 +234,8 @@ const initRblu = () => {
 
               return gsap.fromTo(
                 data.next.container,
-                { clipPath: `circle(3% at ${percentLeft}% ${percentTop + 3}%)` },
-                { clipPath: `circle(140.9% at ${percentLeft}% ${percentTop + 3}%)`, delay: 0.75, duration: 1.1, ease: "power2.inOut", clearProps: "clipPath",
+                { clipPath: `circle(3% at ${percentLeft}% ${percentTop - 3}%)` },
+                { clipPath: `circle(140.9% at ${percentLeft}% ${percentTop - 3}%)`, delay: 0.75, duration: 1.1, ease: "power2.inOut", clearProps: "clipPath",
                 onComplete: () => {
                 window.scrollTo({ top: 0, behavior: 'instant' });
                 $(data.next.container).removeClass("fixed"); }}); 
@@ -283,8 +283,8 @@ const initRblu = () => {
                
               return gsap.fromTo(
                 data.next.container,
-                { clipPath: `circle(3% at ${percentLeft}% ${percentTop + 3}%)` },
-                { clipPath: `circle(140.9% at ${percentLeft}% ${percentTop + 3}%)`, delay: 0.75, duration: 1.1, ease: "power2.inOut", clearProps: "clipPath",
+                { clipPath: `circle(3% at ${percentLeft}% ${percentTop - 3}%)` },
+                { clipPath: `circle(140.9% at ${percentLeft}% ${percentTop - 3}%)`, delay: 0.75, duration: 1.1, ease: "power2.inOut", clearProps: "clipPath",
                 onComplete: () => {
                 window.scrollTo({ top: 0, behavior: 'instant' });
                 $(data.next.container).removeClass("fixed"); }}); 
