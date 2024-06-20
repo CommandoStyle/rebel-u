e5c03570448046cbaad68a7bee418e7f645729ab
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-06-20 14:55:57 +0700

    Update initWebflow.js

--- js/global/initWebflow.js
+++ js/global/initWebflow.js
@@ -1,6 +1,6 @@
 export const initWebflow = () => {
 
-  const pageId = '64217a46f171f96acc4bd03b'; // may need to query data.next.html for data-wf-page attribute
+  const pageId = '64e281d54ecfa09e96f8f5d8'; // may need to query data.next.html for data-wf-page attribute
   document.documentElement.setAttribute('data-wf-page', pageId);
 
   window.Webflow && window.Webflow.destroy();
