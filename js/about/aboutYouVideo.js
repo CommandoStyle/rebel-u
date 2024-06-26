4976cd0ee84c27910bcb737ea399a683faf6817c
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-06-26 18:16:03 +0700

    Update aboutYouVideo.js

--- js/about/aboutYouVideo.js
+++ js/about/aboutYouVideo.js
@@ -88,8 +88,8 @@ function takepic() {
         .set(cursor, {delay: 1, autoAlpha: 1 });
  
 vidBtn.addEventListener("click", () => {
-    //initializeCamera();
-    //video.play();
+    initializeCamera();
+    video.play();
     getPermission();
     showVid.play(0);    
     //setTimeout(takepic, 1700);
@@ -100,8 +100,8 @@ vidBtn.addEventListener("click", () => {
 function getPermission() {
 navigator.permissions.query({ name: "camera" }).then((result) => {
   if (result.state === "granted") {
-    initializeCamera();
-    video.play();   
+    //initializeCamera();
+    //video.play();   
     setTimeout(takepic, 1700);
     setTimeout(stopVideoStream, 3400);
   } else if (result.state === "prompt") {
