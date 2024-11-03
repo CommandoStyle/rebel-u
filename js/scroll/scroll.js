6c11e0280b51aa2cca9c5734a601f97ddb1133e7
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-11-03 18:16:19 +0700

    Update scroll.js

--- js/scroll/scroll.js
+++ js/scroll/scroll.js
@@ -30,7 +30,7 @@ let smoother = ScrollSmoother.create({
 
   // SS stop / start on button hovers
       
-  var ssHoverStop = document.querySelectorAll('.cta-btn, .menu__btn, .nav__logo-btn, .mrebel__submit-query, .nav__items-container, .nav__container, .currency__items-container, .booking-items__container, .currency__btn, .query-items__container, .mrebel-sub__link, .quiz-items__container, .quiz-cta__btn, .subs-privacy__btn, .inline__btn, .share__items-container, .share__backdrop');
+  var ssHoverStop = document.querySelectorAll('.cta-btn, .menu__btn, .nav__logo-btn, .mrebel__submit-query, .nav__items-container, .nav__container, .currency__items-container, .booking-items__container, .currency__btn, .query-items__container, .mrebel-sub__link, .quiz-items__container, .quiz-cta__btn, .subs-privacy__btn, .inline__btn, .share__items-container, .share__backdrop, .vid-start__btn');
       
     ssHoverStop.forEach(function(ssStop) {
          ssStop.addEventListener('mouseover', function hover() { smoother.paused(true); 
