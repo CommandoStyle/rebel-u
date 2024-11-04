1f01b9ff1551b4a6d5f47e47166e34ed7f87f516
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-11-04 14:28:53 +0700

    Update quizGlobal.js

--- js/global/quizGlobal.js
+++ js/global/quizGlobal.js
@@ -121,7 +121,6 @@ quiz_open = gsap.timeline({ paused: true });
 	      .set([".redact-cover-quiz-exit", ".redact-cover-nav"], { scaleX: 0 }, "<") 
 	      .set(".quiz-exit-txt", { autoAlpha: 1 }, "<")
 	      .set(".quiz-menu-txt", { autoAlpha: 0 }, "<")
-	      .set(".redact-cover-nav", { filter: "invert(100%)" }, "<")
               .to(arrowMask, {filter:"invert(100%)", duration: 0.001}, "<")
               .to(quizBg, { scale: 12, transformOrigin: "50% 50%", ease: "power2.inOut", duration: 1.2 })
 	      .set(".touch-print-open__wrap", { autoAlpha: 0 }, "-=0.6")
@@ -177,8 +176,7 @@ quizCancel.forEach((quizCancel) => {
 	 .set(navBg, { autoAlpha: 0 })
          .set(CategoryQuiz, { x: -30, opacity: 0, display: "none" })
          .set(quizCancelinner, { x: -30, opacity: 0 })
-         .set(quizInnerWrap, { opacity: 0, display: "none", x: -20 })
-	 .set(".redact-cover-nav", { filter: "invert(0%)" }, "<");
+         .set(quizInnerWrap, { opacity: 0, display: "none", x: -20 });
 
         })
       });
