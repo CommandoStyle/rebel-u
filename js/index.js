d6766630a9107467be9fc2837713e152b50b9934
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-11-05 21:34:43 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -151,9 +151,26 @@ import { legalLinksSt } from './legal/legalLinksSt.js';
 
 import { errorMarquees } from './error/errorMarquees.js';
 
-
-
 const mmTrans = window.matchMedia("(max-width: 480px)");
+
+function getClickPos() {
+
+setTimeout(() => {
+
+// Get mouse position on click  
+let percentTop;
+let percentLeft;
+$(document).on("click touchstart", function (e) {
+  let mouseTop = e.pageY - $(window).scrollTop();
+  let mouseLeft = e.pageX;
+  percentTop = (mouseTop / $(window).height()) * 100;
+  percentLeft = (mouseLeft / $(window).width()) * 100;
+  return false; 
+});
+
+  }, 0);
+}
+getClickPos();
 
 // Load all functions on first load
 
