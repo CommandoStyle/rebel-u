f27fe1c636c3823b7eefee8869df330db0d85cc1
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-11-07 22:54:18 +0700

    Update homeEbelSwiper.js

--- js/home/homeEbelSwiper.js
+++ js/home/homeEbelSwiper.js
@@ -168,7 +168,7 @@ mm.add("(max-width: 428px)", () => {
   const ebelSwiper = new Swiper ('.swiper.mrebel-text', {
     loop: true,
     speed: 600,
-    autoplay: { delay: 0 },
+    autoplay: { delay: 5400 },
     //autoplay: false,
     on: {
       slideChangeTransitionStart: function () {
