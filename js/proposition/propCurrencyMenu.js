da54521a8c67a91aac739e595786bd718dafd83d
Outsideman69 <62745007+Outsideman69@users.noreply.github.com>
2024-11-08 23:23:07 +0700

    Update propCurrencyMenu.js

--- js/proposition/propCurrencyMenu.js
+++ js/proposition/propCurrencyMenu.js
@@ -423,7 +423,7 @@ currencySelect.forEach((currencySelect) => {
     const currencyContain = document.querySelector('.currency__items-container')
     if (!currencyContain) return
 
-    gsap.set(currencyHole, { borderTopWidth: "990px", borderBottomWidth: "990px", borderRightWidth: "990px", borderLeftWidth: "990px", display: "none", force3D:false })
+    gsap.set(currencyHole, { scale: 0.01, display: "none", rotation: 0, force3D:false })
 
     currencySelect.addEventListener("click", (e) => {
         // Calculate distance between user click and top left corner of button
@@ -447,8 +447,7 @@ currencySelect.forEach((currencySelect) => {
         .to([currRev, ".current-arrow-right__track", ".current-arrow-left__track"], { duration: 0.5, opacity: 0, stagger: 0, ease: "linear" }, "<")
         .to(navBotRev, { x: 30, opacity: 0, ease: "expo.out", duration: 0.3 },"-=0.65")
         .to(".nav-bot-rev-vert", { y: -15, opacity: 0, ease: "expo.out", duration: 0.3 },"<") 
-        //.fromTo(currencyHole, { scale: 0.005454, rotation: 0, force3D:false }, { duration: 1.2, scale: 1, rotation:0.01, force3D:false, ease: "expoScale(0.005454, 1, power1.easeOut)" }, "-=0.3")  
-        .fromTo(currencyHole, { borderTopWidth: "990px", borderBottomWidth: "990px", borderRightWidth: "990px", borderLeftWidth: "990px", force3D:false }, { duration: 1.2, borderTopWidth: "100px", borderBottomWidth: "100px", borderRightWidth: "100px", borderLeftWidth: "100px", force3D:false, ease: power1.easeOut }, "-=0.3")  
+        .fromTo(currencyHole, { scale: 0.01, rotation: 0, force3D:false }, { duration: 1.2, scale: 1, rotation:0.01, force3D:false, ease: "expoScale(0.01, 1, power1.easeOut)" }, "-=0.3")    
         .to('[data-img]', { autoAlpha: 0, duration: 0.3, ease: "sine.inOut" },"-=1.5")
         .to(".home-nav__btn", { autoAlpha: 0, duration: 0.001 }, "-=0.05")
         .to(".nav__logo-btn", { autoAlpha: 1, duration: 0.001 }, "<")
